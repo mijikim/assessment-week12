a8bccf7aaf1fee1d802ba00f22e82622d2315a78
mijikim <miji.kim926@gmail.com>
2014-08-25 10:36:35 -0600

    movies require valid data

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ActiveRecord::Base
 
   validates :name, presence: true
-  validates :year, presence: true
+  validates :year, presence: true, length: { is: 4 }, numericality: { only_integer: true }
   validates :synopsis, presence: true
 
 end
